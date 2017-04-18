1a3a7c20429b2e89ce9905b353a7e2a9581a5489
James Zheng <James@Jamess-iMac.home>
2017-04-17 22:21:56 -0400

    fixed links

--- code/app.js
+++ code/app.js
@@ -1,3 +1,4 @@
+//express setup
 const express = require('express');
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(express.static(publicPath));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-
+//session setup
 const session = require('express-session');
 const sessionOptions = {
 	secret: 'secret cookie thang',
@@ -23,15 +24,115 @@ const sessionOptions = {
 };
 app.use(session(sessionOptions));
 
+//bcrypt setup
+const bcrypt = require('bcrypt');
+
+//bring in database 
 require('./db');
 
+//mongoose setup
 const mongoose = require('mongoose'),
-Item = mongoose.model('Item');
+Item1 = mongoose.model('Item1');
+User = mongoose.model('User');
+Comment = mongoose.model('Comment');
 
-app.get('/', (req, res) => {
-  Item.find({}, (err, items) => {
+//displays index page
+app.get('/', function (req, res) {
+	res.render('index'); 
+});
+
+app.get('/register', function (req, res) {
+	res.render('register');
+});
+
+app.post('/register', (req, res) => {
+	const pw = req.body.password;
+	if (pw.length < 8) {
+		res.render('error', {message: 'pw length issue'});
+	}
+	else {
+		const un = req.body.username;
+		User.findOne({username:un}, (err, result) => {
+			if (err) {
+				console.log(err);
+				res.send('an error occurred, please see the server logs for more information1');	
+			}
+			else if (result) {
+				res.render('error', {message: 'user already exists'});
+			}
+			else {
+				bcrypt.hash(pw, 10, (err, hash) => {
+					if (err) {
+						console.log(err);
+						res.send('an error occurred, please see the server logs for more information2');	
+					}
+					else {
+						const user = new User({
+							username: un,
+							password: hash
+						});
+						user.save((err) => {
+							if (err) {
+								console.log(err);
+								res.send('an error occurred, please see the server logs for more information3');	
+							}
+							else {
+								req.session.regenerate((err) => {
+									if (err) {
+										console.log(err);
+										res.send('an error occurred, please see the server logs for more information4');	
+									}
+									else {
+										req.session.username = user.username;
+										res.redirect('/home');
+									}
+								});
+							}
+						});
+					}
+				});
+			}
+		});
+	}
+});
+
+app.get('/login', function(req, res) {
+	res.render('login');
+});
+
+
+app.post('/login', function(req, res) {
+	User.findOne({username: req.body.username}, (err, user) => {
+		if (!err && user) {
+			bcrypt.compare(req.body.password, user.password, (err, passwordMatch) => {
+				if (passwordMatch) {
+					req.session.regenerate((err) => {
+						if (!err) {
+							req.session.username = user.username;
+							res.redirect('/home'); 
+						} 
+						else {
+							console.log('error'); 
+							res.send('an error occurred, please see the server logs for more information');
+						}
+					});
+				}
+				else {
+					res.render('error', {message: "Incorrect Password"});
+				}
+			});
+		}
+		else {
+			res.render('error', {message: 'user does not exist'});
+		}
+	});
+});
+
+app.get('/home', (req, res) => {
+	Item1.find({}, (err, items) => {
 		if(err) {
-			console.log(err);
+			//console.log(err);
+			res.render('error', {});
 		}
 		else {
 			res.render('home', {items: items});
@@ -39,18 +140,27 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.post('/', (req, res) => {
-	const item = new Item({
-		name: req.body.username
+app.post('/home', (req, res) => {
+	const item = new Item1({
+		name: req.body.name,
+		image: req.body.imageURL,
+		size: req.body.size,
+		condition: req.body.condition,
+		description: req.body.description
 	});
 	item.save((err) => {
 		if(err){
 			console.log(err);
+			res.render('error', {});
 		}
 		else {
-			res.redirect('/');
+			res.redirect('/home');
 		}
 	});
+});
+
+app.get('/user', (req, res)=> {
+	res.render('user', {user: req.session.username});
 });
 
 
--- code/db.js
+++ code/db.js
@@ -1,88 +1,34 @@
-
-/*
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-
-// Users
-const User = new mongoose.Schema({
-  // username, password done through Passport.js
-  listings:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Clip' }]
-});
-
-/*
-Name
-Image of shoe
-Shoe size
-Condition (out of 10)
-*/
-/*
-const Item = new mongoose.Schema({
-	name: String,
-	image: String,
-  size: Number,
-  condition: String,
-  //user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-  //comments: [Comment]
-});
-
-// a Comment
-const Comment = new Schema({
-  text: String,
-  user: String
-});
-
-mongoose.model('Item', Item);
-mongoose.model('Comment', Comment);
-
-
-/*
-// is the environment variable, NODE_ENV, set to PRODUCTION? 
-if (process.env.NODE_ENV === 'PRODUCTION') {
- // if we're in PRODUCTION mode, then read the configration from a file
- // use blocking file io to do this...
- var fs = require('fs');
- var path = require('path');
- var fn = path.join(__dirname, 'config.json');
- var data = fs.readFileSync(fn);
-
- // our configuration file will be in json, so parse it and set the
- // conenction string appropriately!
- var conf = JSON.parse(data);
- var dbconf = conf.dbconf;
-} else {
- // if we're not in PRODUCTION mode, then use
- dbconf = 'mongodb://localhost/jz1257';
-}
-
-mongoose.connect('mongodb://localhost/final-project'); //dbconf
-
-
-*/
-
-const mongoose = require('mongoose');
-   
+//mongoose setup
+const mongoose = require('mongoose'); 
 const urlSlugs = require('mongoose-url-slugs');
 const Schema = mongoose.Schema;
 
+//User schema
+const User = new Schema({
+  username: {type: String, unique: true},
+  password: {type: String, unique: true}
+});
+
+//Comment schema
 const Comment = new Schema({
   text: String,
   user: String
 });
 
-const Item = new mongoose.Schema({
+//Item schema
+const Item1 = new Schema({
   name: String,
-  //image: String,
-  //size: Number,
-  //condition: String,
-  //user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+  image: String,
+  size: String,
+  condition: String,
+  description: String,
   //comments: [Comment]
 });
 
-//Link.plugin(urlSlugs('title'));
-
+mongoose.model('User', User);
+mongoose.model('Item1', Item1);
 mongoose.model('Comment', Comment);
-mongoose.model('Item', Item);
+
 
 
 // is the environment variable, NODE_ENV, set to PRODUCTION? 
